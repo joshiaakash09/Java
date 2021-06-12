8b3d257559765c3e55e8a6d5b22a48a0aee07018
pheonix <aakash_joshi@outlook.com>
2021-06-12 01:47:26 -0500

    Length of the word in string

--- src/AT4/TaskOneFindLength.java
+++ src/AT4/TaskOneFindLength.java
@@ -1,4 +1,42 @@
 package AT4;
 
+import java.util.Scanner;
+
 public class TaskOneFindLength {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input, word="", longword="", smallword="";
+        int len, longlen=0, wordlen=0, smallen;
+        Character ch;
+
+        System.out.println("Enter a sentence");
+
+        input=sc.nextLine().trim();
+        input=input+" ";
+
+        len = input.length();
+        smallen = len;
+
+        for (int i=0; i<len; i++){
+            ch = input.charAt(i);
+            if (ch == ' '){
+                wordlen = word.length();
+                if (wordlen>longlen){
+                    longlen = wordlen;
+                    longword = word;
+                }
+                if (wordlen<smallen){
+                    smallen = wordlen;
+                    smallword = word;
+                }
+                word="";
+            } else{
+                word=word+ch;
+            }
+        }
+        System.out.println("The longest word is :"+longword);
+        System.out.println("The length of the word is :"+longlen);
+        System.out.println("The shortest word is :"+smallword);
+        System.out.println("The length of the word is :"+smallen);
+    }
 }
